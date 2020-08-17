111ee31211e8afa4d985b0f92b845d19b7c586a3
z646930529 <35284110+646930529@users.noreply.github.com>
2020-08-17 15:16:57 +0800

    Add files via upload

--- cvp.py
+++ cvp.py
@@ -8,11 +8,30 @@ from torch.autograd import Variable
 from PIL import Image
 import numpy as np
 import glob
+import cv2
+import time
+import os
+import shutil
+
+
+def removepath(path):
+    time.sleep(0.1)
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    time.sleep(0.1)
+    os.mkdir(path)
+    time.sleep(0.1)
+
+
+removepath('test1')
+removepath('test2')
+removepath('test3')
+removepath('test4')
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-net = models.resnet50(num_classes=3)
-net.load_state_dict(torch.load('./net_099.pth'))
+net = models.resnet152(num_classes=3)
+net.load_state_dict(torch.load('./model/net_100.pth'))
 net.to(device)
 net.eval()
 
@@ -23,14 +42,24 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
 ])
 
-import cv2
+
 with torch.no_grad():
-    cap = cv2.VideoCapture(0)
-    while 1:
-        _, frame = cap.read()
-        #img = Image.open(file)
+    findex = 0
+
+    #cap = cv2.VideoCapture(0)
+    #while 1:
+    #    _, frame = cap.read()
+
+
+    for imgpath in glob.glob('test/*'):
+        frame = cv2.imread(imgpath)
+        imgname = imgpath.replace('\\', '/').split('/')[-1]
+        
+        findex+=1
         img = Image.fromarray(cv2.cvtColor(frame,cv2.COLOR_BGR2RGB))
         imgT = transform_test(img).unsqueeze(0).to(device)
         p = net(imgT).cpu().numpy()
         v = np.argmax(p)
-        print(['photo','phone','normal'][v])
+        print(findex, v)
+
+        cv2.imwrite('test'+str(v+1)+'/'+imgname, frame)
